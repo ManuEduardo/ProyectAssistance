38d01f8de33cf42d69ca7e10e44e9dad3cc19267
Manuel Sanchez Suyon <manueduardo000@hotmail.com>
2022-09-18 03:00:34 -0500

    BinarySearch

--- src/main/java/org/proyect/Controladores/Algoritmos/Busqueda/BinarySearch.java
+++ src/main/java/org/proyect/Controladores/Algoritmos/Busqueda/BinarySearch.java
@@ -1,34 +1,50 @@
 package org.proyect.Controladores.Algoritmos.Busqueda;
-
 import org.proyect.Modelos.Atributo;
-
+import org.proyect.Controladores.Algoritmos.Ordenamiento.BubbleSort;
 public class BinarySearch implements Buscar {
-
-    //IMPORTANTEEEEEEEEEEEEEEEEEEE: NO OLVIDES QUE PARA EL BINARYSEARCH PRIMERO ORDENAS LA LISTA
-    //lo podrias ordenar asi:
-        // primero importas el metodo: import org.proyect.Controladores.Algoritmos.Ordenamiento.BubbleSort;
-        //luego creas la clase: BubbleSort SB = new BubbleSort();
-        //y al final ordenas la lista: SB.ordenamiento(Assistances,"X"); en X pondrias el parametro por el que buscas para
-            //que lo ordenes por esa forma, en minuscula.
-
-    //Se busca en la lista en base al atributo de la clase ingresado como objetivo
-    //en dado caso no se encuentre devolver -1
+    BubbleSort BS = new BubbleSort();
     @Override
     public int busqueda(Atributo[] lista, String tipoObjetivo, String objetivo) throws Exception {
         Object tipo = lista[0].ObtenerAtributo(tipoObjetivo);
         return switch (((Object) tipo).getClass().getSimpleName()) {
-            case "String" -> buscarString(lista, objetivo);
-            case "Integer" -> buscarInt(lista, objetivo);
+            case "String" -> buscarString(lista, tipoObjetivo,objetivo);
+            case "Integer" -> buscarInt(lista, tipoObjetivo, objetivo);
             default -> throw new Exception("Error al programar los parametros");
         };
     }
 
-    private int buscarInt(Atributo[] lista, String objetivo){
-        //implementas el codigo para buscar, guiate de los codigos para ordenar el como
-        //comparamos los int, String y Date y tambien como los convertimos
-        return 0;
+    private int buscarInt(Atributo[] lista, String tipoObjetivo, String objetivo) throws Exception {
+        lista = (Atributo[]) BS.ordenamiento(lista,tipoObjetivo);
+        int inicio = 0;
+        int fin = lista.length - 1;
+        int pos;
+        while (inicio <= fin) {
+            pos = (inicio+fin) / 2;
+            if ( (Integer) lista[pos].ObtenerAtributo(tipoObjetivo) == Integer.parseInt(objetivo) )
+                return pos;
+            else if ( (Integer)lista[pos].ObtenerAtributo(tipoObjetivo) < Integer.parseInt(objetivo)) {
+                inicio = pos+1;
+            } else {
+                fin = pos-1;
+            }
+        }
+        return -1;
     }
-    private int buscarString(Atributo[] lista, String objetivo){
-        return 0;
+    private int buscarString(Atributo[] lista, String tipoObjetivo, String objetivo) throws Exception {
+        lista = (Atributo[]) BS.ordenamiento(lista,tipoObjetivo);
+        int inicio = 0;
+        int fin = lista.length - 1;
+        int pos;
+        while (inicio <= fin) {
+            pos = (inicio+fin) / 2;
+            if (String.valueOf(lista[pos].ObtenerAtributo(tipoObjetivo)).equals(String.valueOf((objetivo))))
+                return pos;
+            else if (String.valueOf(lista[pos].ObtenerAtributo(tipoObjetivo)).compareTo(String.valueOf(objetivo))<0) {
+                inicio = pos+1;
+            } else {
+                fin = pos-1;
+            }
+        }
+        return -1;
     }
 }
