f9cdedab874b2579781b8d950a6e38eb67ef8b75
Manuel Sanchez Suyon <manueduardo000@hotmail.com>
2022-11-17 19:07:48 -0500

    Ventanas Principales

--- src/main/java/org/proyect/Controladores/ControladoresVentanas/ControladorLogin.java
+++ src/main/java/org/proyect/Controladores/ControladoresVentanas/ControladorLogin.java
@@ -1,8 +1,9 @@
-package org.proyect.Controladores;
+package org.proyect.Controladores.ControladoresVentanas;
 
+import org.proyect.Controladores.Controlador;
 import org.proyect.Vistas.Ventana;
 
-public class ControladorLogin extends Controlador{
+public class ControladorLogin extends Controlador {
     public ControladorLogin(Ventana ventana) {
         super(ventana);
     }
---
+++ src/main/java/org/proyect/Controladores/ControladoresVentanas/ControladorMenuPrincipal.java
@@ -0,0 +1,15 @@
+package org.proyect.Controladores.ControladoresVentanas;
+
+import org.proyect.Controladores.Controlador;
+import org.proyect.Vistas.Ventana;
+
+public class ControladorMenuPrincipal extends Controlador {
+    public ControladorMenuPrincipal(Ventana ventana) {
+        super(ventana);
+    }
+
+    @Override
+    public void iniciarVentana() {
+
+    }
+}
